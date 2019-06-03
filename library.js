28369f6b61acc16cd2dc3d84ad75cc53a5fdf5c9
Aldane Purrier <aldanepur@gmail.com>
2019-06-03 15:33:43 -0400

    added ashleydiv

--- library.js
+++ library.js
@@ -17,3 +17,23 @@ for (p in devpokemon) {
 section1.style.display = 'grid'
 section1.style.gridTemplate = 'repeat(1, 500px) / repeat(3, 33%)'
 section1.style.gridGap = '1%'
+
+let section2 = document.getElementById('section2')
+let ashpokemon = ['025','039','115']
+
+for (p in ashpokemon) {
+  let ashleydiv = document.createElement('div')
+  // div.style.backgroundImage = `url('https://www.smashbros.com/assets_v2/img/fighter/${fighters[f]}/main.png')`
+  ashleydiv.style.backgroundImage = `url('https://assets.pokemon.com/assets/cms2/img/pokedex/full/${ashpokemon[p]}.png')`
+  ashleydiv.style.backgroundSize = 'contain'
+  ashleydiv.style.backgroundRepeat = 'no-repeat'
+  ashleydiv.style.border = ''
+  ashleydiv.style.height = '200px'
+  ashleydiv.style.margin = '20%'
+  section2.appendChild(ashleydiv)
+
+}
+
+section2.style.display = 'grid'
+section2.style.gridTemplate = 'repeat(1, 500px) / repeat(3, 33%)'
+section2.style.gridGap = '1%'
