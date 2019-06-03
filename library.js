9da7b3d8ebf3b2b3b2114f2eb8fbf2bd16443032
Devin Guerrero <devinguerrero1015@gmail.com>
2019-06-03 15:27:44 -0400

    Aldain API

--- library.js
+++ library.js
@@ -1,1 +1,19 @@
+let section1 = document.getElementById('section1')
+let pokemon = ['023','095','006']
 
+for (p in pokemon) {
+ let devindiv = document.createElement('div')
+ // div.style.backgroundImage = `url('https://www.smashbros.com/assets_v2/img/fighter/${fighters[f]}/main.png')`
+ devindiv.style.backgroundImage = `url('https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon[p]}.png')`
+ devindiv.style.backgroundSize = 'contain'
+ devindiv.style.backgroundRepeat = 'no-repeat'
+ devindiv.style.border = ''
+ devindiv.style.height = '40%'
+ devindiv.style.margin = '20%'
+ section1.appendChild(devindiv)
+
+}
+
+section1.style.display = 'grid'
+section1.style.gridTemplate = 'repeat(1, 500px) / repeat(3, 33%)'
+section1.style.gridGap = '1%'
