12d140d39f6aaa0f3daffe380e4c54f793afa12e
Aldane Purrier <aldanepur@gmail.com>
2019-06-03 15:31:55 -0400

    added devindiv

--- library.js
+++ library.js
@@ -1,1 +1,19 @@
+let section1 = document.getElementById('section1')
+let devpokemon = ['023','095','006']
 
+for (p in devpokemon) {
+  let devindiv = document.createElement('div')
+  // div.style.backgroundImage = `url('https://www.smashbros.com/assets_v2/img/fighter/${fighters[f]}/main.png')`
+  devindiv.style.backgroundImage = `url('https://assets.pokemon.com/assets/cms2/img/pokedex/full/${devpokemon[p]}.png')`
+  devindiv.style.backgroundSize = 'contain'
+  devindiv.style.backgroundRepeat = 'no-repeat'
+  devindiv.style.border = ''
+  devindiv.style.height = '200px'
+  devindiv.style.margin = '20%'
+  section1.appendChild(devindiv)
+
+}
+
+section1.style.display = 'grid'
+section1.style.gridTemplate = 'repeat(1, 500px) / repeat(3, 33%)'
+section1.style.gridGap = '1%'
