a8e155adae08154e766e69f819a6d2b9ea44fdbe
Mohammad Talha <mtalha210@hotmail.com>
2019-06-03 13:31:37 -0400

    api

--- PokeGym.js
+++ PokeGym.js
@@ -1,1 +1,12 @@
-
+requestAPI = (pokemon, container) => {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let data = JSON.parse(this.responseText)
+          poke = new Pokemon(data["name"],
+           data["stats"][2]["base_stat"],
+           data["stats"][0]["base_stat"],
+           data["stats"][0]["base_stat"],
+           data["abilities"][0]["ability"]["name"],
+           data["sprites"]["front_default"],container)
+        displayUser(poke)
